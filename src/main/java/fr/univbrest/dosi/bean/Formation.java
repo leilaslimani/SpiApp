aa7aa1f3c2bb8115a670b7344fd99db138742b5d
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2018-02-13 12:18:50 +0100

    formation repository

--- src/main/java/fr/univbrest/dosi/bean/Formation.java
+++ src/main/java/fr/univbrest/dosi/bean/Formation.java
@@ -40,6 +40,20 @@ public class Formation implements Serializable {
 
 	public Formation() {
 	}
+	
+	public Formation(String codeFormation, Date debutAccreditation, String diplome, String doubleDiplome,
+			Date finAccreditation, BigDecimal n0Annee, String nomFormation) {
+		super();
+		this.codeFormation = codeFormation;
+		this.debutAccreditation = debutAccreditation;
+		this.diplome = diplome;
+		this.doubleDiplome = doubleDiplome;
+		this.finAccreditation = finAccreditation;
+		this.n0Annee = n0Annee;
+		this.nomFormation = nomFormation;
+	}
+
+
 
 	public String getCodeFormation() {
 		return this.codeFormation;
